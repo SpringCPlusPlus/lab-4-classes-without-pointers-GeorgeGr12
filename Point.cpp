ba0e269eb3ef0720df0826f944a4a2b1b0555cfc
GeorgeGr12 <georgegrammatosis@gmail.com>
2025-03-16 00:14:01 -0400

    LAB4: George Grammatosis - filled in point.cpp and put definitions for rectangle constructors in Rectangle1.cpp

--- Point.cpp
+++ Point.cpp
@@ -3,3 +3,14 @@
 #include "Point.h"
 
 using namespace std;
+
+
+
+Point::Point(double xCoord, double yCoord) : x(xCoord), y(yCoord) {}
+
+double Point::distanceTo(const Point& other) const {
+    return sqrt(pow((other.x - x), 2.0) + pow((other.y - y), 2.0));
+}
+void Point::print() const {
+    cout << "(" << x << ", " << y << ")" << endl;
+}
